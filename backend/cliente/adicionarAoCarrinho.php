d39e7cdd232225f844d0441a8d69d6ee2b88c307
ArturZahn <62709763+ArturZahn@users.noreply.github.com>
2021-06-24 18:15:06 -0300

    carrinho

--- backend/cliente/adicionarAoCarrinho.php
+++ backend/cliente/adicionarAoCarrinho.php
@@ -19,16 +19,16 @@ $itemCarrinho_quantidade = intval($_POST['produtoQuantidade']);
 
 include("../conexao.php");
 
-$query = mysqli_query($con, "SELECT SUM(itemCarrinho_quantidade) AS itemCarrinho_quantidade FROM itemCarrinho WHERE produto_cod = $_POST[produtoCod] AND cliente_cod = $_SESSION[cliente_cod];");
+$query = mysqli_query($con, "SELECT itemCarrinho_quantidade FROM itemCarrinho WHERE produto_cod = $_POST[produtoCod] AND cliente_cod = $_SESSION[cliente_cod] ORDER BY itemCarrinho_cod DESC;");
 
 if(mysqli_num_rows($query) > 0)
 {
     $itemCarrinho_quantidade += intval(mysqli_fetch_object($query)->itemCarrinho_quantidade);
 }
 
-$query = mysqli_query($con, "DELETE FROM itemCarrinho WHERE produto_cod = $_POST[produtoCod] AND cliente_cod = $_SESSION[cliente_cod];");
+$query = mysqli_query($con, "UPDATE itemCarrinho SET itemCarrinho_quantidade = $itemCarrinho_quantidade WHERE produto_cod = $_POST[produtoCod] AND cliente_cod = $_SESSION[cliente_cod];");
 
-$query = mysqli_query($con, "INSERT INTO itemCarrinho (itemCarrinho_quantidade, cliente_cod, produto_cod) VALUES ($itemCarrinho_quantidade, $_SESSION[cliente_cod], $_POST[produtoCod])");
+// $query = mysqli_query($con, "INSERT INTO itemCarrinho (itemCarrinho_quantidade, cliente_cod, produto_cod) VALUES (, $_SESSION[cliente_cod], $_POST[produtoCod])");
 
 echo json_encode(Array("success" => true));
 
--- backend/cliente/atualizarCarrinho.php
+++ backend/cliente/atualizarCarrinho.php
@@ -1,11 +1,40 @@
 <?php
 
-// esse arquivo ainda nao foi programado, foi apenas usado para alguns testes...
+
+session_start();
+if(empty($_SESSION["cliente_cod"])) {
+    echo json_encode(Array("success"=> false)); 
+    die();
+}
+
 
 $json = json_decode(file_get_contents('php://input'));
 
-include("log.php");
+if(empty($json)) {
+    echo json_encode(Array("success"=> false)); 
+    die();
+}
 
-clog(var_export($json, true));
+include("../conexao.php");
+
+foreach ($json as $item) {
+    if(!empty($item->produtoCod))
+    {
+        if(empty($item->quantidade))
+        {
+            $query = mysqli_query($con, "DELETE FROM itemcarrinho WHERE produto_cod = $item->produtoCod AND cliente_cod = $_SESSION[cliente_cod];");
+        }
+        else
+        {
+            $query = mysqli_query($con, "UPDATE itemcarrinho SET itemCarrinho_quantidade = $item->quantidade WHERE produto_cod = $item->produtoCod AND cliente_cod = $_SESSION[cliente_cod];");
+        }
+    }
+}
+
+
+
+// include("log.php");
+
+// clog(var_export($json, true));
 
 ?>
