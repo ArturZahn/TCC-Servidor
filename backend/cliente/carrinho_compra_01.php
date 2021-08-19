32f38adcdb7ff4d81e807e79562afc0dae85072f
ArturZahn <62709763+ArturZahn@users.noreply.github.com>
2021-08-19 15:05:59 -0300

    alteracao escolha de endereco na compra

--- backend/cliente/carrinho_compra_01.php
+++ backend/cliente/carrinho_compra_01.php
@@ -14,6 +14,14 @@
 
     $e = mysqli_fetch_object($query);
 
+    if($e->endereco_cod === null)
+    {
+        echo json_encode(array(
+            'endereco_cod' => null
+        ));
+        die();
+    }
+
     echo json_encode(array(
         'endereco_cod' => $e->endereco_cod,
         'endereco' =>  "$e->endereco_rua, $e->endereco_numero - $e->endereco_bairro, $e->endereco_cidade-$e->endereco_estado"
