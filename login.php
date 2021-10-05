cf6ebbdb39ad311243866d7795346be5c322cf32
Dayana Freitas <dayanaresendefreitas@gmail.com>
2021-10-05 16:09:43 -0300

    login

--- login.php
+++ login.php
@@ -53,7 +53,7 @@
                 <input
                   class="block w-full mt-1 text-sm border-red-600 dark:text-gray-300 dark:bg-gray-700 focus:border-red-400 focus:outline-none focus:shadow-outline-red form-input"
                   placeholder="exemple@email"
-                />
+                />  
               </label>
               <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">
