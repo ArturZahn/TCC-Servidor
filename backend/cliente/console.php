4ad68b3c76a161a733f6674930fc4b1864b3d24c
dayana-freitas <dayanaresendefreitas@gmail.com>
2021-11-23 14:46:13 -0300

    aknmskf

--- backend/cliente/console.php
+++ backend/cliente/console.php
@@ -23,7 +23,7 @@ if(isset($_GET["mostrar"]))
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
